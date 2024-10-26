536fe5a6de9fbf6f7c9e733bcc8b2cab68c1a408
Yazid Dawiss <104469015+whoisyazzy@users.noreply.github.com>
2024-10-26 18:36:19 -0400

    Upd

--- test/src/main/java/com/example/test/controller/CounterController.java
+++ test/src/main/java/com/example/test/controller/CounterController.java
@@ -8,5 +8,11 @@ import org.springframework.http.ResponseEntity;
 
 @RestController
 public class CounterController {
+
+    //GET /counters/
+    // POST /counters
+    // PUT /counters/{counter}
+    // DEL /counters/{counter}
+    // GET /counters/{counter}
     
 }
