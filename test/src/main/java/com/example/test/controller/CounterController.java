5bd7d02facd44b649d521364cf255933a30ad038
Yazid Dawiss <104469015+whoisyazzy@users.noreply.github.com>
2024-10-27 10:51:25 -0400

    Map format

--- test/src/main/java/com/example/test/controller/CounterController.java
+++ test/src/main/java/com/example/test/controller/CounterController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,10 +35,15 @@ public class CounterController {
     }
 
 
-    //GET /counters/  - List of currently active counters in a map/dictionary format ({ "abc": 5, "xyz": 3 })
+    //GET /counters/  - List of currently active counters in a map/dictionary format ({ "abc": 5, "xyz": 3 }) //Key:counter Values:name
     @GetMapping("/counters")
-    public List<Counter> getAllCounters() {
-        return counterService.getAllCounters();
+    public Map<String, Integer> getAllCounters() {
+        Map<String, Integer> allCounters = new HashMap<>();
+        List<Counter> LCounters = counterService.getAllCounters();
+        for (Counter counter : LCounters){
+            allCounters.put(counter.getName(),counter.getCount());
+        }
+        return allCounters;
     }
     
 
@@ -65,7 +72,7 @@ public class CounterController {
 //     - Increases a counter value by one (no body required).
 
 // - Fails if counter does not exist (404 Not found)
-    @PutMapping("counters/{name}")
+    @PutMapping("counters/{id}")
     public ResponseEntity<Counter> updateCounter(@PathVariable Long id, @RequestBody Counter counter){
         Counter updCounter = counterService.updateCounter(id, counter);
         return ResponseEntity.ok(updCounter);
@@ -76,16 +83,16 @@ public class CounterController {
 
      // DEL /counters/{counter} - Decreases a counter value by one, if value <= 0 the counter disappears.
 // - Does fail if the counter does not exist.
-    @DeleteMapping("/counters/{name}")
+    @DeleteMapping("/counters/{id}")
     public ResponseEntity<String> deleteCounter(@PathVariable Long id){
-        counterService.deleteCounter(id)
+        counterService.deleteCounter(id);
         return ResponseEntity.ok("Counter deleted successfully");
     }
 
     // GET /counters/{counter}
 //     - Returns value of counter {"counter1": 5}. // - Fails if the counter does not exist (404 Not found)
 
-    @GetMapping("/counters/{name}")
+    @GetMapping("/counters/{id}")
     public ResponseEntity<Counter> getCounterbyId(@PathVariable Long id) {
         Optional<Counter> counter = counterService.getCounterbyID(id);
         return counter.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
